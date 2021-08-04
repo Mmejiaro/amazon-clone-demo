188dbdf99542b54c35d4912575c013da0da9f2dc
Michael Mejia <michaelmejia1171@gmail.com>
2021-08-04 12:35:20 -0400

    inital setup for react

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBdXHFDOZdvJxfwFZ2kclIiLMSVRgUkh1o",
+    authDomain: "clone-3516a.firebaseapp.com",
+    projectId: "clone-3516a",
+    storageBucket: "clone-3516a.appspot.com",
+    messagingSenderId: "540248047128",
+    appId: "1:540248047128:web:492a9dc8b6e2aa55edcd6d",
+    measurementId: "G-DF6G5M7GPQ"
+  };
