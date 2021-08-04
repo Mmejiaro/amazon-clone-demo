47cd581efb6d9de2365eb6a0c2b6d8c90cebc895
Michael Mejia <michaelmejia1171@gmail.com>
2021-08-04 13:33:01 -0400

    initialized Header for amazon-clone

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
       <h1>Hello, Let's build the amazon clone</h1>
+      {/* Header */}
+      <Header />
+      {/* Home */}
     </div>
   );
 }
