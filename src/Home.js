e1be98a409e1885576577985a9bb3b6df3c071a8
Michael Mejia <michaelmejia1171@gmail.com>
2021-08-05 09:35:43 -0400

    amazon background banner created and styled

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
 function Home() {
     return (
         <div className="home">
-            <p>I am the home component</p>
+            <div className="home_container">
+                <img className="home_image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="amazon banner" />
+
+            </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
